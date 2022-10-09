307301358eb0bd04900acef3e4fb1648a0abfc4d
nuthan.s <nss@icims.com>
2022-10-09 23:07:03 +0530

    email addition test

---
+++ contact.js
@@ -0,0 +1,25 @@
+function sendEmail() {
+  
+//   Email.send({
+//     Host : "smtp.elasticemail.com",
+//     Username : "ssnuthan001@gmail.com",
+//     Password : "A933DFF92B7DE02E9666FE68E060E2AC3C97",
+//     To : 'ssnuthan001@gmail.com',
+//     From : "nuthanss1996@gmail.com",
+//     Subject : "This is the subject",
+//     Body : "And this is the body"
+// }).then(
+//   message => alert(message)
+// );
+
+console.log();
+Email.send({
+  SecureToken : "a02d981a-d89f-420b-91a5-ef9963749154",
+  To : 'ssnuthan001@gmail.com',
+  From : "nuthanss1996@gmail.com",
+  Subject : "This is the subject",
+  Body : "And this is the body"
+}).then(
+message => alert(message)
+);
+}
